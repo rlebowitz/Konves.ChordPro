4dff3f0f01e93e9319edf53d2fef12ac1f104182
Rob Lebowitz <lebowitz@finaltouch.com>
2022-04-06 09:24:29 -0500

    fixes

--- ChordPro.Lib/ChordProSerializer.cs
+++ ChordPro.Lib/ChordProSerializer.cs
@@ -1,9 +1,5 @@
 ﻿using ChordPro.Lib.DirectiveHandlers;
 using ChordPro.Lib.Directives;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 
 namespace ChordPro.Lib
 {
@@ -14,28 +10,36 @@ namespace ChordPro.Lib
             return Deserialize(stream, null);
         }
 
-        public static Document Deserialize(Stream stream, IEnumerable<DirectiveHandler> customHandlers)
+        public static Document Deserialize(Stream stream, IEnumerable<DirectiveHandler>? customHandlers)
         {
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                Parser parser = new Parser(reader);
-                return new Document(parser.Parse());
-            }
+            using StreamReader reader = new(stream);
+            Parser parser = new(reader);
+            return new Document(parser.Parse());
         }
 
-        public static void Serialize(Document document, TextWriter writer, SerializerSettings settings = null)
+        public static void Serialize(Document document, TextWriter writer, SerializerSettings settings)
         {
+            Guard.NotNull(document);
+            Guard.NotNull(writer);
+            Guard.NotNull(settings);
             var index = DirectiveHandlerUtility.GetHandlersDictionaryByType(settings?.CustomHandlers);
 
             foreach (ILine line in document.Lines)
             {
-                if (line is Directive)
-                    writer.WriteLine(index[line.GetType()].GetString(line as Directive, settings?.ShortenDirectives == true)); // TODO: harden
-                else if (line is SongLine)
-                    // writer.WriteLine(line.ToString()); // TODO: fix
-                    Write(writer, line as SongLine);
-                else if (line is TabLine)
-                    writer.WriteLine((line as TabLine).Text);
+                if (line == null)
+                {
+                    continue;
+                }
+                else if (line is Directive directive) { 
+                    writer.WriteLine(index[line.GetType()].GetString(directive, settings?.ShortenDirectives == true)); 
+                }
+                else if (line is SongLine songLine)
+                {
+                    Write(writer, songLine);
+                }
+                else if (line is TabLine tabLine) { 
+                    writer.WriteLine(tabLine.Text);
+                }
                 else
                     throw new ArgumentException("unknown line type");
             }
@@ -50,21 +54,18 @@ namespace ChordPro.Lib
                 if (addSpace)
                     writer.Write(' ');
 
-                if (block is Word)
+                if (block is Word word)
                 {
-                    Word word = block as Word;
-
                     foreach (Syllable syllable in word.Syllables)
                     {
-                        if (syllable.Chord != null)
+                        if (syllable.Chord != null) { 
                             writer.Write($"[{syllable.Chord.Text}]");
-
+                        }
                         writer.Write(syllable.Text);
                     }
                 }
-                else if (block is Chord)
+                else if (block is Chord chord)
                 {
-                    Chord chord = block as Chord;
                     writer.Write($"[{chord.Text}]");
                 }
 
--- ChordPro.Lib/DirectiveComponents.cs
+++ ChordPro.Lib/DirectiveComponents.cs
@@ -5,6 +5,12 @@ namespace ChordPro.Lib
 {
     public sealed class DirectiveComponents
     {
+        public string Key { get; private set; }
+        public string SubKey { get; private set; }
+        public string Value { get; private set; }
+        private static Regex DirectiveRegex { get; } = new Regex(@"^\s*{\s*(?<key>[^\s:}]+)(?:\s+(?<subkey>[^:}]*))?(?:\:(?<value>[^}]+))?}\s*", RegexOptions.Compiled);
+
+       
         public DirectiveComponents(string key, string subKey, string value)
         {
             Key = key;
@@ -14,35 +20,32 @@ namespace ChordPro.Lib
 
         public static DirectiveComponents Parse(string s)
         {
-            DirectiveComponents components;
-            if (TryParse(s, out components))
-                return components;
+            if (TryParse(s, out DirectiveComponents components)) { 
+                return components; 
+            }
 
             throw new FormatException("Directive is not in the format {key[ subkey][:value]}");
         }
 
-        public static bool TryParse(string s, out DirectiveComponents components)
+        public static bool TryParse(string s, out DirectiveComponents? components)
         {
-            Match match = _directiveRegex.Match(s);
+            Match match = DirectiveRegex.Match(s);
 
             if (match == null || string.IsNullOrWhiteSpace(match.Groups["key"].Value))
             {
                 components = null;
                 return false;
             }
-
-            components = new DirectiveComponents(
-                key: match.Groups["key"]?.Value.ToLower().Trim(),
-                subKey: match.Groups["subkey"]?.Value.Trim(),
-                value: match.Groups["value"]?.Value.Trim());
+            components = new DirectiveComponents
+            {
+                Key = match.Groups["key"]?.Value.ToLower().Trim(),
+                SubKey = match.Groups["subkey"]?.Value.Trim(),
+                Value = match.Groups["value"]?.Value.Trim()
+            };
 
             return true;
         }
 
-        public string Key { get; }
-        public string SubKey { get; }
-        public string Value { get; }
-
-        static readonly Regex _directiveRegex = new Regex(@"^\s*{\s*(?<key>[^\s:}]+)(?:\s+(?<subkey>[^:}]*))?(?:\:(?<value>[^}]+))?}\s*", RegexOptions.Compiled);
+       
     }
 }
